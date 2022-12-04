fa45b02f1732ebb8cdb78d0d5d9ea495cdd4f525
Senne <senne.de.neef@student.ehb.be>
2022-12-04 14:45:19 +0100

    commit android project

---
+++ app/src/main/java/be/senne/android_basic_demo/MainActivity.kt
@@ -0,0 +1,40 @@
+package be.senne.android_basic_demo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import be.senne.android_basic_demo.ui.theme.Android_basic_demoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Android_basic_demoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Android_basic_demoTheme {
+        Greeting("Android")
+    }
+}
